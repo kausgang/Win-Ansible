d9bbd5f7065ae7f5779c3eeeb482b59828472164
kausgang <write.to.kaustav@gmail.com>
2021-04-17 22:16:33 -0400

    updated readme

--- README.md
+++ README.md
@@ -24,5 +24,5 @@ cd Win-Ansible
 - Create Target machine based on the image requirement
   - Setup passwordless authentication between the controller and target.
 - Setup Target machine as default Ansible host for the Controller machine.
-- Extract privatekey of Controller and Target 
-  - Use username = vagrant and the private key to login using SSH Client softwares (Putty/MobaXterm) 
+- Extract privatekey of Controller and Target in *connection-setting* folder (generated after first run)
+  - Use username = **vagrant** and the private key to login using SSH Client softwares (Putty/MobaXterm) 
--- setup.bat
+++ setup.bat
@@ -5,6 +5,7 @@ vagrant up
 vagrant ssh controller -c "ssh-keyscan -H target >> /home/vagrant/.ssh/known_hosts"
 
 @REM extract the private keys for ssh connection
-md putty-connection-setting
-copy .vagrant\machines\controller\virtualbox\private_key .\putty-connection-setting\controller-private_key.ppk
-copy .vagrant\machines\target\virtualbox\private_key .\putty-connection-setting\target-private_key.ppk
+md connection-setting
+copy .vagrant\machines\controller\virtualbox\private_key .\connection-setting\controller-private_key.ppk
+copy .vagrant\machines\target\virtualbox\private_key .\connection-setting\target-private_key.ppk
+
